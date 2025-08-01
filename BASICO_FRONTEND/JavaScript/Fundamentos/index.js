f7639dfa26b545b86da177cca679e72c9a3d86ed
Lenizio27 <lenizio27@gmail.com>
2025-08-01 17:13:57 -0300

    estilização conteudo JS
    estudo de javascript, estruturação de conteudo e  aplicação de estilos.

---
+++ BASICO_FRONTEND/JavaScript/Fundamentos/index.js
@@ -0,0 +1,19 @@
+// tipos de dados e variavei em js
+
+var n1 = 10;
+const n2 = "String";
+let n3 = true;
+
+// hosting
+carregar(n2)
+
+// Objeto
+let pessoas = {}
+
+// Array
+let notas = [1,2,3,4];
+
+// Função
+function carregar(texto){
+    console.log(texto);
+};
